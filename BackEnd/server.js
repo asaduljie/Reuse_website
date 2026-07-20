98abff69599ff7a9f186c912601b3031205032de
asaduljie <asaduljie123@gmail.com>
2026-07-20 18:17:24 +0800

    Configure Express backend for Vercel Serverless deployment using vercel.json

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -31,8 +31,11 @@ app.use("/api/products", productRoutes);
 
 app.use("/api/orders", orderRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `Server running on port ${process.env.PORT}`
-  );
-});
+if (process.env.NODE_ENV !== "production") {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+module.exports = app;
